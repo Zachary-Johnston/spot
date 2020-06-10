966bb7a08b7b4786f1ce96b12a635108cd4162e2
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-06-09 20:08:08 -0700

    Update rest_server.py

--- spot/spot-master/rest/src/rest_server.py
+++ spot/spot-master/rest/src/rest_server.py
@@ -11,52 +11,15 @@ import time
 import paho.mqtt.client as mqtt
 
 
-                ##############################################################################
-                #                 Including completed code from assignment API
-                ##############################################################################
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']
 db_host = os.environ['MYSQL_HOST']
 
 
-def coffeeset(req):
-  # View the Dictionary that was Posted
-  # Get the fname
-  print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-  info = req.json_body
-  print(info)
-
-  msg = json.dumps(info)
-  print(msg)
-  # SEND TO RASPBERRY PI WITH MQTT
-  client = mqtt.Client("JJJ")
-  client.connect("polarcoffee.org", port=1883, keepalive=60, bind_address="")
-  client.publish("test", msg)
-  
-  records = {}
-  records['success'] = True
-  records = Response(body=json.dumps(records))
-  records.headers.update({'Access-Control-Allow-Origin': '*',})
-
-  return records
-
 if __name__ == '__main__':
   config = Configurator()
 
-  config.add_route('coffeeset', '/coffeeset') # Added route for timer
-  config.add_view(coffeeset, route_name='coffeeset')
-  
-
-              ##############################################################################
-              #                        Adding Routes to Database                           #
-              ##############################################################################
-
-
-
-              ##############################################################################
-              #                        --END OF CODE ---                                   #
-              ##############################################################################
   
 
   app = config.make_wsgi_app()
